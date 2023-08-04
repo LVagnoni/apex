cb9547d6785e81f3dfb6dc718d7aaa539dbbd9cf
LVagnoni <lisa.vagnoni@gmail.com>
2023-08-04 14:04:24 -0400

    img
    upload

---
+++ img/logo/support.php
@@ -0,0 +1,349 @@
+<?php
+include('headerc.php');
+include_once('assests/analyticstracking.php');
+
+?>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<link href="css/main.css" rel="stylesheet" type="text/css">
+<link rel="icon" href="favicon.ico" type="image/ico" />
+
+<link rel="stylesheet" type="text/css" href="css/slide.css" />
+<script src="js/jquery-1.4.2.min.js"></script>
+<script src="/lib/w3.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<style type="text/css">
+
+a.morelink {
+    text-decoration:none;
+    outline: none;
+}
+.morecontent span {
+    display: none;
+}
+.comment {
+    width: 900px;
+   
+}
+
+.grid
+{
+border:1px;
+border-color: #CCCCCC;
+border-style: solid;
+border-collapse:collapse;
+border-width: 1px;
+"width:300px;
+}
+
+
+th { vertical-align: top }
+td { vertical-align: top;
+}
+table   { 
+border-collapse: collapse;}
+
+ul{
+  margin-left:16px;
+}
+
+.colum {
+    -webkit-column-count: 3; /* Chrome, Safari, Opera */
+    -moz-column-count: 3; /* Firefox */
+    column-count: 3;
+}
+
+</style>
+
+
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<title>apexart :: Support Us</title>
+</head>
+
+<body>
+
+<table width="954" border="0" align="center">
+  <tr valign="top">
+    <td align="center" valign="top"><table width="941" border="0" bgcolor="#FFFFFF">
+      
+      
+  <tr valign="top">
+    <td style="padding: 15px;"><span class="mainText"><strong><em>We need your support more than ever!</em></strong></span><br /><br /> <span class="descript">Your donation is helping to sustain our international fellowships, exhibitions, writers' reading series, books, and public programs. <br />
+          <br />
+          Giving even $10 means a lot and makes a big difference! Not-for-profit organizations retain their tax status by showing they have support from a large number of people giving a wide range of amounts. Your support tells them you want us to keep doing what we're doing. <br />
+          <br /> <span class="descript">apexart is a 501c3 organization. Contributions are tax-deductible to the extent provided by law.<br />
+          <br /> 
+          
+          Thank you.<br />  <br /> </span>
+        
+        <table width="918" border="0" cellspacing="0" cellpadding="0">
+          <tr><div style="margin-right: 40px; margin-left: 40px;"><td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</td>
+                <td>
+                  <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                  <input type="hidden" name="cmd" value="_donations" />
+                  <input type="hidden" name="business" value="FD6SQY9RV79Z6" />
+                  <input type="hidden" name="currency_code" value="USD" />
+                  <input type="hidden" name="amount" value="10" />
+                  <input type="image" src="https://www.apexart.org/images/button/10.png" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                  <img alt="" border="0" src="https://www.paypal.com/en_US/i/scr/pixel.gif" width="1" height="1" />
+                  </form>
+                  </td>
+
+                  <td>
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                    <input type="hidden" name="cmd" value="_s-xclick">
+                    <input type="hidden" name="hosted_button_id" value="R87687Y98DUUL">
+                    <input type="image" src="https://apexart.org/images/button/25.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                    </form>
+                  </td>
+
+                  <td>
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                    <input type="hidden" name="cmd" value="_s-xclick">
+                    <input type="hidden" name="hosted_button_id" value="NE5S9JQBH2ZA2">
+                    <input type="image" src="https://apexart.org/images/button/50.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                    </form>
+                  </td>
+                  
+                  
+                  <td>
+                    <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                    <input type="hidden" name="cmd" value="_s-xclick">
+                    <input type="hidden" name="hosted_button_id" value="9JM4RBPF5ZX36">
+                    <input type="image" src="https://apexart.org/images/button/100.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                    <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                    </form>
+                  </td>
+
+                    <td>
+                      <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                      <input type="hidden" name="cmd" value="_s-xclick">
+                      <input type="hidden" name="hosted_button_id" value="EDME2D6MNB7FY">
+                      <input type="image" src="https://apexart.org/images/button/250.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                      <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                      </form>
+                    </td>                   
+
+                     <td>
+                     <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+                     <input type="hidden" name="cmd" value="_s-xclick">
+                     <input type="hidden" name="hosted_button_id" value="B2J6KTXC82ZX2">
+                     <input type="image" src="https://apexart.org/images/button/open.png" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
+                     <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
+                     </form>
+                 </td>
+
+              </tr>
+
+          </div><tr>
+
+        </table>
+      <br />If you prefer to donate by mail or over the phone, please contact the <strong>Director of Development</strong> at <strong>212-431-5270</strong>.</span>   <span id="siteseal"><script type="text/javascript" src="https://seal.godaddy.com/getSeal?sealID=08RaroXIHztsRI1OwxeBQ6d3KTglvvJaBjkOO6hRAW7MioAvgMPSUgltXfPH"></script></span>  </td>  
+    <tr valign="top">
+      <td style="padding: 15px;" class="standardText">
+     <span class="standardTextLg"><strong>apexart Funders</strong></span><br><br>
+     <div class="colum"><strong>FOUNDATIONS<br />
+          </strong>
+          291 Foundation<br />
+          Affirmation Arts Fund<br />
+          The Andy Warhol Foundation<br />for the Visual Arts<br />
+          The Buhl Foundation<br />
+          Degenstein Foundation<br />
+          Fifth Floor Foundation<br />
+          Foundation for Contemporary Arts (FCA)<br />
+          Genie and Donald Rice Charitable Trust<br />
+          The Greenwich Collection Ltd.<br />
+          Jewish Communal Fund<br />
+          Kenneth A. Cowin Foundation<br />
+          Lambent Foundation Fund of Tides Foundation<br />
+          The Milton and Sally Avery Arts Foundation<br />
+          The Mindel Family Charitable Annuity Trust<br />
+          Trust for Mutual Understanding<br />
+          Wender Weis Foundation for Children<br />
+          William Talbott Hillman Foundation<br />
+          
+          
+          <br />
+         <strong>GOVERNMENT<br />
+            </strong>National Endowment for the Arts<br />
+            Public Funds from New York City Department of Cultural Affairs in partnership with the City Council<br />
+            New York State Council on the Arts with the support of Governor Andrew M. Cuomo and the New York State Legislature
+  <br />
+  <br />
+          
+          <strong>FOREIGN AGENCIES/INSTITUTIONS<br />
+            </strong> Artis Grant Recipient <br />
+            Austrian Cultural Forum in New York<br />
+            British Council<br />
+            Consejo Nacional para la Cultura y las Artes (CONACULTA)<br />
+            Consulate General of Hungary in New York<br />
+            Consulate General of the Netherlands in New York<br />
+            Cultural Services of the French Embassy in New York<br />
+            Danish Arts Council<br />
+            Etant donnés Foundation<br />
+            FRAME Finnish Fund for Art Exchange<br />
+            Hungarian Cultural Center<br />
+            Mexican Cultural Institute of New York<br />
+            Mondriaan Foundation<br />
+            New York - Israel Cultural Cooperation Commission<br />
+            Nordic Culture Fund<br />
+            Office of Cultural Affairs at the Consulate General of Israel in New York <br />
+            Pro Helvetia<br />
+            Seoul Art Space Geumcheon<br />
+            Swedish Arts Grants Committee<br />
+            Turkish Cultural Foundation
+          <br /><br />
+          <strong>CORPORATE<br />
+            </strong>A Gathering of the Tribes <br />
+            AKV Berlin<br />
+            Charlotte Art League<br />
+            Fashion Institute of Technology<br />
+            Fence Magazine<br />
+            Gotham Vapor Lounge LLC<br />
+            J.W. Allen Insurance Company<br />
+            Jack Shainman Gallery<br />
+            Residency Unlimited<br />	
+            Rhode Island School of Design<br />
+            Stephen Lasar Architects<br />
+            West Wind Studios <br /><br />
+            
+            <strong>INDIVIDUALS</strong><br />
+            Terry Agriss<br />
+            Atif Akin<br />
+            Morana Alac<br />
+            George Alexiou<br />
+            Nathaniel Andreini<br />
+            Nathalie Angles<br />
+            Uri Auerbach<br />
+            Yona Backer<br />
+            Barbara Bantivoglio<br />
+            Christophe Barbeau<br />
+            Jillian Bergman<br />
+            Julie Blankenship<br />            
+            Scott Breakstone<br />
+            Kathryn and David Brenner<br />
+            Rebecca Brown<br />
+            Spencer Brownstone<br />
+            Marie Burns<br />
+            Melissa Rachleff Burtt<br />
+            Nathan Catlin<br />
+            David Cantoni<br />
+            Thomas Carroll<br />
+            Hilary Chasse<br />
+            Jeffry Chiplis<br />
+            Sherman Clarke<br />
+            Barrie Cline<br />
+            Seth Cluett<br />
+            Shannon Connelly<br />
+            Maureen Connor<br />
+            Kenneth Cowin<br />
+            Tara Cronin<br />
+            Will Dean<br />
+            Rachel Dedman<br />
+            Allan deSouza<br />
+            Fred Dewey<br />
+            Cristina Doi<br />
+            Jackie Du<br />
+            Ciara Ennis<br />
+            Nicky Enright<br />
+            Margaret Ewing<br />
+            Heather Felty<br />
+            Margot Fox<br />
+            Gwylene Gallimard<br />
+            Chloe Geoghegan<br />
+            Brooke and Joshua Gibbons<br />
+            Julian Gilbert-Davis<br />
+            Lisa Tung and Spencer Glendon<br />
+            Rachel Rosenberg and Paul Goldstein<br />
+            E.W. Goldstein<br />
+            Alice Adams Gordy<br />
+            Emily and Bill Gottlieb<br />
+            Chelsea Guerdat<br />
+            Adler Guerrier<br />
+            Abigail Hahn<br />
+            Benjamin Heidersberger<br />
+            Tracey Henry<br />
+            Eddie Hsiung<br/>
+            Millree Hughes<br />
+            Astrid Hustvedt and Jon Kessler<br />
+            Patricia Jacomella<br />
+            Nina Katchadourian and Sina Najafi<br />
+            Paula and Thomas Keltner<br />
+            Lynn Knezevich<br />
+            Julia Knight<br />
+            Joseph Krings<br />
+            Jane and Howard Kramer<br />
+            Carin Kuoni<br />
+            Angela Lamonte<br />
+            Elizabeth Larison<br />
+            Nancy and Stephen Lasar<br />
+            Robert Lawrence<br />
+            Raymond Learsy<br />
+            Tracy Leavitt<br />
+            Jenna London<br />
+            Ellen and David Levy<br />
+            Carrie Mackin<br />
+            Kalpana Maddikunta<br />
+            Lenore Malen<br />
+            Christopher Manzione<br />
+            Joan Mathews<br />
+            Judith Miller<br />
+            Michael J. Miller<br />
+            Susette Min<br />
+            Susan and Joel Mindel<br />
+            Arlene Mintzer<br />
+            Rosemary O'Neill<br />
+            Joan Pachner and Richard Newman<br />
+            Katerina Nikou<br />
+            Stella Padnos<br />
+            Mark Pasek<br />
+            James Pierce<br />
+            Jody Pinto<br />
+            Catya Plate<br />
+            Michael Pribich<br />
+            Melissa Rachleff Burtt<br />
+            Jo Ractliffe<br />
+            Phyllis Rand<br />
+            Amie Block Ratajczak<br />
+            Johannah Rodgers<br />
+            Anne Rorimer<br />
+            Kay Rosen<br />
+            Katherine Rubinstein<br />
+            Wendy and Nick Rubinstein<br />
+            Susan and Frederic Rubinstein<br />
+            Kerri Schlottman<br />
+            Barbara and Howard Simowitz<br />
+            Walter Strachowsky<br />
+            Tind<br />
+            Jacqueline Thaw<br />
+            Robert Thill<br />
+            Victoria Tiley<br />
+            Taguhi Torosyan<br />
+            Kay Turner<br />
+            Amy Wender-Hoch and John Hoch<br />
+            Gregory Williams<br />
+            Lloyd Williams<br />
+            Louise Woehrle<br />
+            Suzan Woodruff and Bruce Bauman<br />
+            Fei Wu<br />
+            Natasha Yannacanedo<br />
+            Wong Siu Yin<br />
+            Sarah Zolt-Gilburne<br />
+            Kathryn Zox
+            
+            and others</div></td>
+    </TABLE></td>
+      </tr>
+    </table>
+    
+    
+    <?php
+include('footerc.php');
+
+?>
+</body>
+</html>
+
+
